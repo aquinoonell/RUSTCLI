eb1d1a4980246eb7a700f37cd95506ec7ecce47c
don <onellaquino@icloud.com>
2025-08-22 14:26:17 -0400

    Go

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, ArgMatches, command};
+use clap::{Arg, ArgAction, ArgMatches, Command, arg, command};
 
 fn main() {
     let match_result: ArgMatches = command!()
@@ -15,6 +15,11 @@ fn main() {
                 .long("last-name")
                 .alias("lname")
                 .required(true),
+        )
+        .subcommand(
+            Command::new("test")
+                .about("does testing things")
+                .arg(arg!(-l --list "list test values").action(ArgAction::SetTrue)),
         )
         .get_matches();
 }
