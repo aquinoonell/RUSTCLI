c873ad14c23b5f1e3289561a71ccba3b2557ddb3
don <onellaquino@icloud.com>
2025-08-21 22:21:44 -0400

    batman

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ use clap::{Arg, ArgMatches, command};
 
 fn main() {
     let match_result: ArgMatches = command!()
-        .arg(Arg::new("firstname").short('f').long("first-name")
-            .required(true).aliases("fname"))
-        .arg(Arg::new("lastname").short('l').long("last-name")
-            .required(true))
+        .arg(
+            Arg::new("firstname")
+                .short('f')
+                .long("first-name")
+                .required(true)
+                .alias("fname"),
+        )
+        .arg(
+            Arg::new("lastname")
+                .short('l')
+                .long("last-name")
+                .required(true),
+        )
         .get_matches();
 }
