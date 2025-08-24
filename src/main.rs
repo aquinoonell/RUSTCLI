3388b9f15c84b0f6c4b1b7d4185c8aafc22f705e
don <onellaquino@icloud.com>
2025-08-24 11:44:19 -0400

    Debug mode added

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ fn main() {
                 .alias("lname")
                 .required(true),
         )
+        .arg(arg!(
+            -d --debug ... "Turn debugging information on"
+        ))
         .subcommand(
             Command::new("test")
                 .about("does testing things")
@@ -25,11 +28,18 @@ fn main() {
 
     match matches
         .get_one::<u8>("debug")
-        .expect("Counts are defaulted") {
-
-        0 => println!("Debug mode is off")
-        1 => println!("Debug mode is kind of on")
-        2 => println!("Debug mode is on")
-        _ => println!("Don't be crazy")
+        .expect("Counts are defaulted")
+    {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+    if let Some(matches) = matches.subcommand_matches("test") {
+        if matches.get_flag("list") {
+            println!("Printing testing lists...");
+        } else {
+            println!("Not printing testing lists..");
+        }
     }
 }
